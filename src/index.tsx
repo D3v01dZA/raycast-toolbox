b2d3f516ea575135c9ade1502957aaa29c7ae4e6
Christopher Altona <caltona1@gmail.com>
2025-01-18 16:40:20 -0500

    Publish

--- src/index.tsx
+++ src/index.tsx
@@ -3,72 +3,70 @@ import { useEffect, useState } from "react";
 import moment from "moment";
 
 interface Detail {
-  value: string,
-  type: string,
+  value: string;
+  type: string;
 }
 
 export default function Command() {
   const [text, setText] = useState("");
-  const [results, setResults] = useState<Detail[]>([])
+  const [results, setResults] = useState<Detail[]>([]);
 
   useEffect(() => {
-    let results: Detail[] = []
+    let results: Detail[] = [];
     if (text != "") {
-      let time = timeResults(text);
+      const time = timeResults(text);
       results = results.concat(time);
     }
-    setResults(results)
-  }, [text])
+    setResults(results);
+  }, [text]);
 
   return (
-    <List
-      onSearchTextChange={setText}
-      searchBarPlaceholder="Convert something..."
-      throttle
-    >
-      {
-        results.map((result, index) => (
-          <List.Item
-            key={index}
-            title={result.type}
-            actions={<ActionPanel title="#1 in raycast/extensions"><Action.CopyToClipboard title="Copy To Clipboard" content={result.value} /></ActionPanel>}
-            accessories={[{ text: result.value }]}
-          />
-        ))
-      }
+    <List onSearchTextChange={setText} searchBarPlaceholder="Convert something..." throttle>
+      {results.map((result, index) => (
+        <List.Item
+          key={index}
+          title={result.type}
+          actions={
+            <ActionPanel title="#1 in raycast/extensions">
+              <Action.CopyToClipboard title="Copy to Clipboard" content={result.value} />
+            </ActionPanel>
+          }
+          accessories={[{ text: result.value }]}
+        />
+      ))}
     </List>
   );
 }
 
 function timeResults(text: string): Detail[] {
-  let results: Detail[] = []
+  const results: Detail[] = [];
 
   function addDate(date: moment.Moment, isNow: boolean) {
-    results.push({ value: date.utc().toISOString(), type: "Date UTC" })
-    results.push({ value: date.local().toISOString(true), type: `Date` })
-    results.push({ value: date.unix() + "", type: "Epoch" })
-    results.push({ value: date.valueOf() + "", type: "Epoch Millis" })
+    results.push({ value: date.utc().toISOString(), type: "Date UTC" });
+    results.push({ value: date.local().toISOString(true), type: `Date` });
+    results.push({ value: date.unix() + "", type: "Epoch" });
+    results.push({ value: date.valueOf() + "", type: "Epoch Millis" });
     if (!isNow) {
-      results.push({ value: date.fromNow(), type: "Difference" })
-      let difference = moment.duration(date.diff(moment()))
-      results.push({ value: difference.asMilliseconds() + "", type: "Difference Milliseconds" })
-      results.push({ value: Math.round(difference.asSeconds()) + "", type: "Difference Seconds" })
-      results.push({ value: Math.round(difference.asMinutes()) + "", type: "Difference Minutes" })
-      results.push({ value: Math.round(difference.asHours()) + "", type: "Difference Hours" })
-      results.push({ value: Math.round(difference.asDays()) + "", type: "Difference Days" })
-      results.push({ value: Math.round(difference.asMonths()) + "", type: "Difference Months" })
-      results.push({ value: Math.round(difference.asYears()) + "", type: "Difference Years" })
+      results.push({ value: date.fromNow(), type: "Difference" });
+      const difference = moment.duration(date.diff(moment()));
+      results.push({ value: difference.asMilliseconds() + "", type: "Difference Milliseconds" });
+      results.push({ value: Math.round(difference.asSeconds()) + "", type: "Difference Seconds" });
+      results.push({ value: Math.round(difference.asMinutes()) + "", type: "Difference Minutes" });
+      results.push({ value: Math.round(difference.asHours()) + "", type: "Difference Hours" });
+      results.push({ value: Math.round(difference.asDays()) + "", type: "Difference Days" });
+      results.push({ value: Math.round(difference.asMonths()) + "", type: "Difference Months" });
+      results.push({ value: Math.round(difference.asYears()) + "", type: "Difference Years" });
     }
   }
 
   if (text.length === 10) {
-    let epoch = Number(text + "000")
+    const epoch = Number(text + "000");
     if (!isNaN(epoch)) {
       addDate(moment(epoch), false);
     }
   }
   if (text.length === 13) {
-    let epoch = Number(text)
+    const epoch = Number(text);
     if (!isNaN(epoch)) {
       addDate(moment(epoch), false);
     }
@@ -76,7 +74,7 @@ function timeResults(text: string): Detail[] {
   if (text.toLocaleLowerCase() === "now") {
     addDate(moment(), true);
   }
-  let other = moment(text)
+  const other = moment(text);
   if (other.isValid()) {
     addDate(other, false);
   }
